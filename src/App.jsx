b07e5bfda663eb39e8c9dfd6a6567eb0f7b97ef0
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-28 16:51:58 -0500

    hero props

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,19 @@
 import React from "react";
-import Advertisement from "./components/advertisement";
-import Product from "./components/product";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import styles from "./App.module.css";
+import Hero from "./components/hero";
 function App() {
   return (
-    <>
-      <Header />
-      <div className={styles.propsMainBody}>
-        <div className={styles.componentBody}>
-          <Advertisement
-            title="Check out our special offers!"
-            description="Do not miss out on this limited deals on our amazing products. 
-        This is a life time deals will be available on our website"
+    <dev>
+      
+          <Hero
+            title="Welcome to our electronics shop website"
+            subtitle="We sell old and new electronits devices upto 50% off"
           />
-          <Advertisement
-            title="Introducing the Ultimate Smartphone!"
-            description="Experience the future with our cutting-edge smartphone. 
-        Stunning design, lightning-fast performance, and a camera that captures 
-        memories like never before. Upgrade your life today!"
+          <Hero
+            title="New Iphone 15"
+            subtitle="Checkout our new Iphone 15 just get release"
           />
-          <Advertisement
-            title="Unleash Your Creative Genius!"
-            description="Elevate your artistry with our professional-grade drawing tablet. Precision, sensitivity, and limitless possibilities at your fingertips. 
-        Unleash your creativity and create masterpieces like never before!"
-          />
-          <Product
-            item_name="iPhone 15"
-            imageUrl="https://cdn.wccftech.com/wp-content/uploads/2023/09/iPhone-15-Pro-2-2-728x394.jpg"
-            item_description="black color"
-            backgroundColor="#ffcc00"
-          />
-        </div>
-      </div>
-      <Footer />
-    </>
+          
+    </dev>
   );
 }
 
--- src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "../styles/footer.module.css";
-function Footer() {
-  return <footer>Copy Right</footer>;
-}
-
-export default Footer;
