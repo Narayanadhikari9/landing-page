d162ed3ac77b6916f12e9d42277316b8f90ae0c6
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-27 20:40:28 -0700

    fixed lining

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,6 @@ function App() {
       <Header />
       <div className={styles.propsMainBody}>
         <div className={styles.componentBody}>
-          <Product
-        item_name="iPhone 15"
-        imageUrl="https://cdn.wccftech.com/wp-content/uploads/2023/09/iPhone-15-Pro-2-2-728x394.jpg"
-        item_description="black color"
-        backgroundColor="#ffcc00"
-      />
           <Advertisement
             title="Check out our special offers!"
             description="Do not miss out on this limited deals on our amazing products. 
@@ -31,6 +25,12 @@ function App() {
             title="Unleash Your Creative Genius!"
             description="Elevate your artistry with our professional-grade drawing tablet. Precision, sensitivity, and limitless possibilities at your fingertips. 
         Unleash your creativity and create masterpieces like never before!"
+          />
+          <Product
+            item_name="iPhone 15"
+            imageUrl="https://cdn.wccftech.com/wp-content/uploads/2023/09/iPhone-15-Pro-2-2-728x394.jpg"
+            item_description="black color"
+            backgroundColor="#ffcc00"
           />
         </div>
       </div>
