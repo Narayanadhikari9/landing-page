683b72768b9a1fe6d50ffac1df3d41ea79f76d73
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-27 18:13:46 -0700

    made adv, footer, header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-
-import Hero from "./components/Hero";
-
+import Advertisement from "./components/advertisement";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import styles from "./App.module.css";
 function App() {
   return (
-    <div>
-      <Hero
-        title="Welcome to our website"
-        subtitle="Discover amazing content"
-        backgroundColor="#f0f0f0"
-      />
-      <Hero
-        title="New arrivals"
-        subtitle="Check out our latest products"
-        backgroundColor="#ffcc00"
-      />
-    </div>
+    <>
+      <Header />
+      <div className={styles.propsMainBody}>
+        <div className={styles.componentBody}>
+          <Advertisement
+            title="Check out our special offers!"
+            description="Do not miss out on this limited deals on our amazing products. 
+        This is a life time deals will be available on our website"
+          />
+          <Advertisement
+            title="Introducing the Ultimate Smartphone!"
+            description="Experience the future with our cutting-edge smartphone. 
+        Stunning design, lightning-fast performance, and a camera that captures 
+        memories like never before. Upgrade your life today!"
+          />
+          <Advertisement
+            title="Unleash Your Creative Genius!"
+            description="Elevate your artistry with our professional-grade drawing tablet. Precision, sensitivity, and limitless possibilities at your fingertips. 
+        Unleash your creativity and create masterpieces like never before!"
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
